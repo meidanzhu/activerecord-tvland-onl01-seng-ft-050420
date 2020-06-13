47b9104aaf839e3a9859a7671165dda957061426
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-13 04:11:48 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,17 +1,17 @@
 class Actor < ActiveRecord::Base
-  has_many :characters 
+  has_many :characters
   has_many :shows, through: :characters
-  
-  def full_name 
+
+  def full_name
     name = self.first_name + " " + self.last_name
   end
-  
-  def list_roles 
-    arr = [] 
+
+  def list_roles
+    arr = []
     self.characters.each do |character|
       str = "#{character.name} - #{character.show.name}"
-      arr << str 
+      arr << str
     end
-    arr 
+    arr
   end
 end
