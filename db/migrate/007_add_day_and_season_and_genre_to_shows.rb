e615ac68615b50e66e694622bdd0844a890cd7fd
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-13 04:23:01 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_and_season_and_genre_to_shows.rb
+++ db/migrate/007_add_day_and_season_and_genre_to_shows.rb
@@ -1,4 +1,4 @@
-class AddDayAndSeasonAndGenreToShows < ActiveRecord::Migration
+class AddDayAndSeasonAndGenreToShows < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
